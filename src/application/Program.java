ef7965c21afd53372dae971d1c6f0e81c11794cd
Emerson Aquino <emersonde.a.s.a.s@gmail.com>
2023-11-03 22:16:01 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		ChessMatch chaessMatch = new ChessMatch();
-		UI.printeBoard(chaessMatch.getPieces());
+		Scanner emerson = new Scanner(System.in);
+		ChessMatch chassMatch = new ChessMatch();
+		
+		while (true) {
+		UI.printeBoard(chassMatch.getPieces());
+		System.out.println();
+		System.out.print("Source: ");
+		ChessPosition source = UI.readChessPosition(emerson);
+		
+		System.out.println();
+		System.out.print("Target:  ");
+		ChessPosition target = UI.readChessPosition(emerson);
+		
+		ChessPiece capturedPiece = chassMatch.performChessMove(source, target);
+		}
 	}
 
 }
