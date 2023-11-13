2ff8d3b1cdc58816e483d13ff25ebd0895c63ec3
Emerson Aquino <emersonde.a.s.a.s@gmail.com>
2023-11-13 15:27:36 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,7 +41,11 @@ public class Program {
 			
 			if (chessMatch.getPromotes() != null) {
 				System.out.print("Enter piece for promotion (B/N/R/Q)");
-				String type = emerson.nextLine();
+				String type = emerson.nextLine().toUpperCase();
+				while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+					System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q)");
+					type = emerson.nextLine().toUpperCase();
+				}
 				chessMatch.replacePromotedPiece(type);
 			}
 		  }
